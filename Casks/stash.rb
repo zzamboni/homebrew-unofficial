dab74ecfef66b5a9092e3c8356bb90a7902d9eae
Caleb Xu <calebcenter@live.com>
2014-02-19 17:20:44 -0500

    Added Stash
    Adding some packages initially to bootstrap the creation process of
    this repository. Expect at least a couple of more packages after this.
    
    ---
    
    A quick Apple Documentation Viewer
    
    How does stash work? Simple, you push cmnd-ctrl-space (or any key combo
    of your choice), Stash's window pops up, you type your search, you get
    documentation, fast!
    
    Couldn't be simpler.

---
+++ Casks/stash.rb
@@ -0,0 +1,7 @@
+class Stash < Cask
+  url 'https://github.com/alehouse/Stash/raw/master/downloads/Stash_1.0.tgz'
+  homepage 'https://github.com/beelsebob/Stash'
+  version '1.0'
+  sha256 'd9f1e125e72ca61e3ba3fffc9b0ff920a9a1e4e48c223c555facf95686af2803'
+  link 'Stash.app'
+end
