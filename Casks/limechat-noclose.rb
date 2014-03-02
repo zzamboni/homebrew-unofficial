f2bedb2a467f12f3e8dbea47cd0f5dcc784dcfa7
Caleb Xu <calebcenter@live.com>
2014-03-02 14:31:30 -0500

    Changing LimeChat link to in-house download

--- Casks/limechat-noclose.rb
+++ Casks/limechat-noclose.rb
@@ -1,7 +1,7 @@
 class LimechatNoclose < Cask
-  url 'https://github.com/rolandwalker/limechat/releases/download/2.39/LimeChat.app.zip'
+  url 'https://github.com/alehouse/limechat/raw/master/downloads/LimeChat_2.39.tgz'
   homepage 'https://github.com/alehouse/limechat'
   version '2.39'
-  sha256 'ae92be383afacaf363eeb65136d487ecd13d404f3c1d1de5b602adef66930429'
+  sha256 'fb4b07e7c1d57786874ba7c0635c219c57cf689a00b5f7134d4ab046c8cf9cbc'
   link 'LimeChat.app'
 end
