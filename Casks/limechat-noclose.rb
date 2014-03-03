0e802464cebd57573579d992b964c15b371ccfa4
Caleb Xu <calebcenter@live.com>
2014-03-03 15:07:37 -0500

    Modified Limechat-noclose to point homepage to rolandwalker's page, not alehouse; added WiiScale

--- Casks/limechat-noclose.rb
+++ Casks/limechat-noclose.rb
@@ -1,6 +1,6 @@
 class LimechatNoclose < Cask
   url 'https://github.com/alehouse/limechat/raw/master/downloads/LimeChat_2.39.tgz'
-  homepage 'https://github.com/alehouse/limechat'
+  homepage 'https://github.com/rolandwalker/limechat'
   version '2.39'
   sha256 'fb4b07e7c1d57786874ba7c0635c219c57cf689a00b5f7134d4ab046c8cf9cbc'
   link 'LimeChat.app'
---
+++ Casks/wiiscale.rb
@@ -0,0 +1,7 @@
+class Wiiscale < Cask
+  url 'https://github.com/alehouse/wiiscale/raw/master/downloads/WiiScale_1.0.tgz'
+  homepage 'https://github.com/damariei/wiiscale'
+  version '1.0'
+  sha256 '072ff61b9daf01cb209cd5e3c7b6eca676ca3a578d6cd3b48f804fd719399cfe'
+  link 'WiiScale.app'
+end
