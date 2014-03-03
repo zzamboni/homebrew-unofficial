2ca381e74369ae8fbd2825d99b3027d548d37b2a
Caleb Xu <calebcenter@live.com>
2014-03-03 16:36:02 -0500

    Added d3visualizer 1.0

---
+++ Casks/d3visualizer.rb
@@ -0,0 +1,7 @@
+class D3visualizer < Cask
+  url 'https://github.com/alehouse/D3Visualizer/raw/master/downloads/D3Visualizer_1.0.tgz'
+  homepage 'https://github.com/swisspol/D3Visualizer'
+  version '1.0'
+  sha256 '5f895f9822e2f5534755d7e4402ea794953720e82270e1006af2971ef5b9e98b'
+  link 'D3Visualizer.app'
+end
