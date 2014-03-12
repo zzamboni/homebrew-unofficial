7bfbae65bca4e40967182118217f356b206afa16
Caleb Xu <caleb@caleb.local>
2014-03-11 21:57:41 -0400

    Added CocoaSlideShow v0.6

---
+++ Casks/cocoaslideshow.rb
@@ -0,0 +1,7 @@
+class Cocoaslideshow < Cask
+  url 'https://github.com/alehouse/CocoaSlideShow/raw/master/downloads/CocoaSlideShow_0.6.tgz'
+  homepage 'https://github.com/nst/CocoaSlideShow'
+  version '0.6'
+  sha256 'e930c05fc8cca61c72bb0821534d84438ba3f0e627f8d162a2a8e4392ef36331'
+  link 'CocoaSlideShow.app'
+end
