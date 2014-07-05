e9e4a79eb2ce73ec8c0f75e0bd353ec7393c9195
marudor <marudor@marudor.de>
2014-07-05 22:34:33 +0200

    Added Dockdrop 1.5.3
    App to upload via ftp/webdav/scp by dropping the file onto the icon

---
+++ Casks/dockdrop.rb
@@ -0,0 +1,9 @@
+class Dockdrop < Cask
+  version '1.5.3'
+  sha256 '34d29b02a02e84abed6121024e4b0a1a5e8bdaf1d568fffa7684bc72f76b75dc'
+
+  url "https://marudor.de/cask/dockdrop/Dockdrop-#{version}.dmg"
+  homepage 'https://web.archive.org/web/20121204080344/http://dockdropx.com/'
+
+  link 'Dockdrop.app'
+end
