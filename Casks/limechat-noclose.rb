d86c887022cff3f3de027f65ee3990b2cd3bc32b
Michael Ford <fanquake@gmail.com>
2014-09-05 16:28:30 +0800

    Format Limechat Noclose

--- Casks/limechat-noclose.rb
+++ Casks/limechat-noclose.rb
@@ -1,7 +1,9 @@
 class LimechatNoclose < Cask
-  url 'https://github.com/rolandwalker/limechat/releases/download/2.42-hide-window-on-close/LimeChat.app.zip'
-  homepage 'https://github.com/rolandwalker/limechat'
   version '2.42'
   sha256 '804497d75f229ae87fdc7d6f050d1662c20b12f73c68e7750c37665ca93223fe'
+
+  url 'https://github.com/rolandwalker/limechat/releases/download/2.42-hide-window-on-close/LimeChat.app.zip'
+  homepage 'https://github.com/rolandwalker/limechat'
+
   link 'LimeChat.app'
 end
