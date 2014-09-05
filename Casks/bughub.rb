4642a32f1e5280e884432bef542224e8b28795c4
Michael Ford <fanquake@gmail.com>
2014-09-05 16:26:36 +0800

    Format Bughub

--- Casks/bughub.rb
+++ Casks/bughub.rb
@@ -1,7 +1,9 @@
 class Bughub < Cask
-  url 'https://github.com/alehouse/BugHub/raw/master/downloads/BugHub_1.0.1.tgz'
-  homepage 'https://github.com/Me1000/BugHub'
   version '1.0.1'
   sha256 'd5fa398386b55ea6881be46e858c12dc906579d07d62fbc7bd5f4d03dd2fa1fd'
+
+  url 'https://github.com/alehouse/BugHub/raw/master/downloads/BugHub_1.0.1.tgz'
+  homepage 'https://github.com/Me1000/BugHub'
+
   link 'BugHub.app'
 end
