e60a15cff09c1182bb91c516a25c49a945542e63
Adam Russell <adamlr6+pub@gmail.com>
2014-09-14 13:57:36 +0530

    Update Dwarf Fortress MacNewbie to 0.9.9.

--- Casks/dwarf-fortress-macnewbie.rb
+++ Casks/dwarf-fortress-macnewbie.rb
@@ -1,6 +1,6 @@
 class DwarfFortressMacnewbie < Cask
-  version '0.9.8a'
-  sha256 '863cc4736f69fba53599027e7cab42f9401e4963be1bca83773627b0ce7520ab'
+  version '0.9.9'
+  sha256 'ca04045341330e2258e1eedf296eefa2b5fe339c7a01618449f4b1adf6478358'
 
   url "http://dffd.wimbli.com/download.php?id=7922&f=Macnewbie_#{version}.dmg"
   homepage 'http://www.bay12forums.com/smf/index.php?topic=128960'
