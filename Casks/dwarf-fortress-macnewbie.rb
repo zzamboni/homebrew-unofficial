2a569d98ebc8f5b4e37efc8468c763654b0f8894
Roland Walker <walker@pobox.com>
2014-09-16 11:26:29 -0400

    use new `suite` stanza in dwarf-fortress-macnewbie

--- Casks/dwarf-fortress-macnewbie.rb
+++ Casks/dwarf-fortress-macnewbie.rb
@@ -5,7 +5,7 @@ class DwarfFortressMacnewbie < Cask
   url "http://dffd.wimbli.com/download.php?id=7922&f=Macnewbie_#{version}.dmg"
   homepage 'http://www.bay12forums.com/smf/index.php?topic=128960'
 
-  link 'Macnewbie'
+  suite 'Macnewbie'
   caveats do
     puts <<-EOS.undent
       The cask '#{@cask}' depends on the casks java and xquartz so
