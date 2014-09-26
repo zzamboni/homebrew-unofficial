88717509dcbaa1837d61558d484641ee9568588f
Roland Walker <walker@pobox.com>
2014-09-26 08:18:53 -0400

    re-use version in stash

--- Casks/stash.rb
+++ Casks/stash.rb
@@ -2,7 +2,7 @@ class Stash < Cask
   version '1.0'
   sha256 'd9f1e125e72ca61e3ba3fffc9b0ff920a9a1e4e48c223c555facf95686af2803'
 
-  url 'https://github.com/alehouse/Stash/raw/master/downloads/Stash_1.0.tgz'
+  url "https://github.com/alehouse/Stash/raw/master/downloads/Stash_#{version}.tgz"
   homepage 'https://github.com/beelsebob/Stash'
 
   app 'Stash.app'
