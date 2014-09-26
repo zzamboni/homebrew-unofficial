1c1bfcfe1d783bf7a033b136eda28252b399c871
Roland Walker <walker@pobox.com>
2014-09-26 08:18:52 -0400

    re-use version in cocoaslideshow

--- Casks/cocoaslideshow.rb
+++ Casks/cocoaslideshow.rb
@@ -2,7 +2,7 @@ class Cocoaslideshow < Cask
   version '0.6'
   sha256 'e930c05fc8cca61c72bb0821534d84438ba3f0e627f8d162a2a8e4392ef36331'
 
-  url 'https://github.com/alehouse/CocoaSlideShow/raw/master/downloads/CocoaSlideShow_0.6.tgz'
+  url "https://github.com/alehouse/CocoaSlideShow/raw/master/downloads/CocoaSlideShow_#{version}.tgz"
   homepage 'https://github.com/nst/CocoaSlideShow'
 
   app 'CocoaSlideShow.app'
