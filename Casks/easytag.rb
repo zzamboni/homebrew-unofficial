ffcedc389dee571cc93deff4194c55787956ff30
Roland Walker <walker@pobox.com>
2014-09-26 08:18:52 -0400

    re-use version in easytag

--- Casks/easytag.rb
+++ Casks/easytag.rb
@@ -2,7 +2,7 @@ class Easytag < Cask
   version '2.1.8-mac-alpha-1'
   sha256 'e637033759396911ed3988fd9349657a6db151821fb21759560ffa220fa3a877'
 
-  url 'https://github.com/rfw/easytag-mac/releases/download/v2.1.8-mac-alpha-1/easytag-v2.1.8-mac-alpha-1.dmg'
+  url "https://github.com/rfw/easytag-mac/releases/download/v#{version}/easytag-v#{version}.dmg"
   homepage 'http://rfw.name/easytag-mac/'
 
   app 'EasyTAG.app'
