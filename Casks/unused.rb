4052908b9dec09057715078de7cc21a20fa41d15
Roland Walker <walker@pobox.com>
2014-09-26 08:18:53 -0400

    re-use version in unused

--- Casks/unused.rb
+++ Casks/unused.rb
@@ -2,7 +2,7 @@ class Unused < Cask
   version '1.1'
   sha256 '8b94c245babbac14198b07df81fdc393b4d7aa9f8a39ffecec79e6645d3f4243'
 
-  url 'https://github.com/alehouse/Unused/raw/master/downloads/Unused_1.1.tgz'
+  url "https://github.com/alehouse/Unused/raw/master/downloads/Unused_#{version}.tgz"
   homepage 'https://github.com/jeffhodnett/Unused'
 
   app 'Unused.app'
