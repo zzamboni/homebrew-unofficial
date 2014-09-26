29dd9cd75c2e4d16d68766868f47779a6cdfc284
Roland Walker <walker@pobox.com>
2014-09-26 08:18:52 -0400

    re-use version in d3visualizer

--- Casks/d3visualizer.rb
+++ Casks/d3visualizer.rb
@@ -2,7 +2,7 @@ class D3visualizer < Cask
   version '1.0'
   sha256 '5f895f9822e2f5534755d7e4402ea794953720e82270e1006af2971ef5b9e98b'
 
-  url 'https://github.com/alehouse/D3Visualizer/raw/master/downloads/D3Visualizer_1.0.tgz'
+  url "https://github.com/alehouse/D3Visualizer/raw/master/downloads/D3Visualizer_#{version}.tgz"
   homepage 'https://github.com/swisspol/D3Visualizer'
 
   app 'D3Visualizer.app'
